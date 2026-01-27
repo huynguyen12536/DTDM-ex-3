c8c410d529c1b4222ce120bc6557ab9f07bf4e52
Nguyễn Huy <huynguyen12536@gmail.com>
2026-01-27 22:28:51 +0700

    Fix nginx.conf server_name and update implementation plan for CloudFront

---
+++ project_TMDT/interview.java
@@ -0,0 +1,32 @@
+class solution{
+    public void swapint(int[] arr, int b, int c){
+        int temp = arr[b];
+        arr[b] =  arr[c];
+        arr[c] = temp;
+
+    }
+
+    public void printarray(int[] arr){
+        for (int i =0; i < arr.length ; i++){
+            system.out.println("["+ arr[i]+"]");
+        }
+    }
+
+    public void rotate(int[] arr, int k){
+        for (int i=0; i< arr.length; i++){
+            
+        }
+    }
+}
+
+class main{
+    public static void main(String[] args){
+        
+    }
+}
+
+
+1 2 3 4 5 6 7
+7 1 2 3 4 5 6
+6 7 1 2 3 4 5
+5 6 7 1 2 3 4
